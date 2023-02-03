f8c69d2c363b6a831aa8419ff55b97d03b5d4cf3
Shehroz <116956168+rshehroz1@users.noreply.github.com>
2023-02-03 19:10:21 +0500

    starter weather-cli
    ob havo dasturini noshladim

---
+++ index.js
@@ -0,0 +1,17 @@
+const getArgs = require("./helpers/args")
+const startCli = ()=>{
+    const args = getArgs(process.argv)
+    console.log(args);
+    if(args.h){
+        // help
+    }
+
+    if(args.s){
+        // save
+    }
+
+    if(args.t){
+        // token
+    }
+}
+startCli()
